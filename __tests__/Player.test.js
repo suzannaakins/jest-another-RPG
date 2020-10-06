f80eb6d531b3b02604e3d86bb3e13ae4a654ce0a
Garlic Fiend <suzannaakins@gmail.com>
2020-10-06 14:38:20 -0700

    test and build player's attack + potion methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -65,3 +65,33 @@ test("subtracts from player's health", () => {
     expect(player.health).toBe(0);
 });
 
+//verify that a player's attack value is within range
+test("gets player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+//check that potion was added correctly
+test('adds a potion to player inventory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+//ensure that usePotion() removes correct potion from inventory
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
+});
+
